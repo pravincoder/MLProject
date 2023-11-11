f756eaa4ff2788323f7c0970ceb86d8051f40f85
pravin <59168712+pravincoder@users.noreply.github.com>
2023-11-11 21:08:37 +0530

    Added Some Hyper Parameter's

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_obj
+from src.utils import save_object
 import os
 
 
@@ -108,7 +108,7 @@ class DataTransformation:
 
             logging.info(f"Saved preprocessing object.")
 
-            save_obj(
+            save_object(
 
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
--- src/utils.py
+++ src/utils.py
@@ -1,36 +1,62 @@
 import os
 import sys
-import dill
-import numpy as np
-import pandas as pd
-from sklearn.metrics import r2_score
-from src.exception import CustomException
-from src.logger import logging
 
-def save_obj(file_path,obj):
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
-        logging.info(f'File Creation of Started....')
-        with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
-        logging.info(f'{file_obj.name} File is Created....')
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
-
-
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-        report ={}
+        report = {}
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train,y_train)  #Training Model
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
+
             y_test_pred = model.predict(X_test)
-            train_model_score = r2_score(y_train,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
             report[list(models.keys())[i]] = test_model_score
+
         return report
-    
+
     except Exception as e:
-        CustomException(e,sys)
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
